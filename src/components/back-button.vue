a30ac483f3ed3adc62a8d4d3e294b815de53d5f0
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-12-24 22:49:03 +0100

    chore: back button component

---
+++ src/components/back-button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import { Button } from '@vifui/core'
+</script>
+
+<template>
+  <Button variant="outline" href="/">
+    <Icon icon="gravity-ui:arrow-left" class="mr-2" />
+    Back to Home
+  </Button>
+</template>
