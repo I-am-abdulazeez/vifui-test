991d007011bfbfd222b7c666264a753885769b98
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-12-24 22:21:58 +0100

    chore: added home and button view

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,18 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/home-view.vue')
+    },
+    {
+      path: '/button',
+      name: 'button',
+      component: () => import('@/views/button-view.vue')
+    }
+  ],
 })
 
 export default router
