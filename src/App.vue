d66927768db6eb32c73c7d3a3bb01e8f428fb182
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-12-25 18:37:04 +0100

    chore: conditionally render backbutton on every page except home page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
+import BackButton from './components/back-button.vue'
+const route = useRoute()
 </script>
 
 <template>
+  <div v-if="route.name !== 'home'" class="max-w-7xl mx-auto py-6">
+    <BackButton />
+  </div>
   <RouterView />
 </template>
 
