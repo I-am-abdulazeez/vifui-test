602781aff8205686c13af8348f312916836b7c22
AbdulAzeez <abdulazeezolamide50@gmail.com>
2026-01-19 11:20:50 +0100

    fix: standardize route formatting and update path for skeleton component

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,88 +6,93 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('@/views/home-view.vue')
+      component: () => import('@/views/home-view.vue'),
     },
     {
       path: '/accordion',
       name: 'accordion',
-      component: () => import('@/views/accordion-view.vue')
+      component: () => import('@/views/accordion-view.vue'),
     },
     {
       path: '/alert',
       name: 'alert',
-      component: () => import('@/views/alert-view.vue')
+      component: () => import('@/views/alert-view.vue'),
     },
     {
-      path: "/avatar",
-      name: "avatar",
-      component: () => import("@/views/avatar-view.vue"),
+      path: '/avatar',
+      name: 'avatar',
+      component: () => import('@/views/avatar-view.vue'),
     },
     {
       path: '/button',
       name: 'button',
-      component: () => import('@/views/button-view.vue')
+      component: () => import('@/views/button-view.vue'),
     },
     {
       path: '/card',
       name: 'card',
-      component: () => import('@/views/card-view.vue')
+      component: () => import('@/views/card-view.vue'),
     },
     {
       path: '/checkbox',
       name: 'checkbox',
-      component: () => import('@/views/check-box.vue')
+      component: () => import('@/views/check-box.vue'),
     },
     {
-      path: "/badge",
-      name: "badge",
-      component: () => import("@/views/badge-view.vue"),
+      path: '/badge',
+      name: 'badge',
+      component: () => import('@/views/badge-view.vue'),
     },
     {
-      path: "/breadcrumbs",
-      name: "breadcrumbs",
-      component: () => import("@/views/bread-crumbs.vue"),
+      path: '/breadcrumbs',
+      name: 'breadcrumbs',
+      component: () => import('@/views/bread-crumbs.vue'),
     },
     {
       path: '/divider',
       name: 'divider',
-      component: () => import('@/views/divider-view.vue')
+      component: () => import('@/views/divider-view.vue'),
     },
     {
       path: '/list',
       name: 'list',
-      component: () => import('@/views/list-view.vue')
+      component: () => import('@/views/list-view.vue'),
     },
     {
       path: '/popover',
       name: 'popover',
-      component: () => import('@/views/popover-view.vue')
+      component: () => import('@/views/popover-view.vue'),
     },
     {
       path: '/tabs',
       name: 'tabs',
-      component: () => import('@/views/tabs-view.vue')
+      component: () => import('@/views/tabs-view.vue'),
     },
     {
       path: '/tooltip',
       name: 'tooltip',
-      component: () => import('@/views/tooltip-view.vue')
+      component: () => import('@/views/tooltip-view.vue'),
     },
     {
-      path: "/spinner",
-      name: "spinner",
-      component: () => import('@/views/spinner-view.vue')
+      path: '/skeleton',
+      name: 'skeleton',
+      component: () => import('@/views/skeleton-view.vue'),
+    },
+    {
+      path: '/spinner',
+      name: 'spinner',
+      component: () => import('@/views/spinner-view.vue'),
     },
     {
       path: '/switch',
       name: 'switch',
-      component: () => import('@/views/switch-view.vue')
+      component: () => import('@/views/switch-view.vue'),
     },
     {
       path: '/upload',
       name: 'upload',
-      component: () => import('@/views/upload-view.vue')
-    }
+      component: () => import('@/views/upload-view.vue'),
+    },
   ],
 })
 
